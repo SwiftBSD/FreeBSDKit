b60d1ed30aaae0b27fa7ff4e43cdff3670de6df7
klheard <koryheard@icloud.com>
2026-01-08 17:47:08 -0600

    Added socket descriptors

--- Sources/Descriptors/SocketDescriptor.swift
+++ Sources/Descriptors/SocketDescriptor.swift
@@ -27,7 +27,7 @@ import Glibc
 import Foundation
 import FreeBSDKit
 
-/// A socket descriptor protocol
+
 public protocol SocketDescriptor: StreamDescriptor, ~Copyable {
     static func socket(domain: Int32, type: Int32, proto: Int32) throws -> Self
     func bind(address: UnsafePointer<sockaddr>, addrlen: socklen_t) throws
@@ -37,10 +37,14 @@ public protocol SocketDescriptor: StreamDescriptor, ~Copyable {
     func shutdown(how: Int32) throws
     func send(_ data: Data, flags: Int32) throws -> Int
     func recv(count: Int, flags: Int32) throws -> Data
+    func sendDescriptors<D: StreamDescriptor>(_ descriptors: [D], payload: Data) throws
+    func recvDescriptors(maxDescriptors: Int, bufferSize: Int) throws -> (Data, [Int32])
 }
 
-public extension SocketDescriptor where Self: ~Copyable  {
-    /// Default listen implementation
+// MARK: - Default implementations
+
+public extension SocketDescriptor where Self: ~Copyable {
+
     func listen(backlog: Int32 = 128) throws {
         try self.unsafe { fd in
             guard Glibc.listen(fd, backlog) == 0 else {
@@ -49,7 +53,6 @@ public extension SocketDescriptor where Self: ~Copyable  {
         }
     }
 
-    /// Default shutdown implementation
     func shutdown(how: Int32) throws {
         try self.unsafe { fd in
             guard Glibc.shutdown(fd, how) == 0 else {
@@ -58,7 +61,6 @@ public extension SocketDescriptor where Self: ~Copyable  {
         }
     }
 
-    /// Create a new socket.
     static func socket(domain: Int32, type: Int32, proto: Int32) throws -> Self {
         let rawFD = Glibc.socket(domain, type, proto)
         guard rawFD >= 0 else {
@@ -67,7 +69,6 @@ public extension SocketDescriptor where Self: ~Copyable  {
         return Self(rawFD)
     }
 
-    /// Bind the socket to the given address.
     func bind(address: UnsafePointer<sockaddr>, addrlen: socklen_t) throws {
         try self.unsafe { fd in
             guard Glibc.bind(fd, address, addrlen) == 0 else {
@@ -77,17 +78,15 @@ public extension SocketDescriptor where Self: ~Copyable  {
     }
 
     func accept() throws -> Self {
-    return try self.unsafe { fd in
-        let newFD = Glibc.accept(fd, nil, nil)
-        guard newFD >= 0 else {
-            throw POSIXError(POSIXErrorCode(rawValue: errno)!)
+        return try self.unsafe { fd in
+            let newFD = Glibc.accept(fd, nil, nil)
+            guard newFD >= 0 else {
+                throw POSIXError(POSIXErrorCode(rawValue: errno)!)
+            }
+            return Self(newFD)
         }
-        return Self(newFD)
     }
-}
 
-
-    /// Connect this socket to a remote address.
     func connect(address: UnsafePointer<sockaddr>, addrlen: socklen_t) throws {
         try self.unsafe { fd in
             guard Glibc.connect(fd, address, addrlen) == 0 else {
@@ -96,7 +95,6 @@ public extension SocketDescriptor where Self: ~Copyable  {
         }
     }
 
-    /// Send data on this connected socket.
     func send(_ data: Data, flags: Int32) throws -> Int {
         return try self.unsafe { fd in
             let bytesSent = data.withUnsafeBytes { ptr in
@@ -109,7 +107,6 @@ public extension SocketDescriptor where Self: ~Copyable  {
         }
     }
 
-    /// Receive data from this connected socket.
     func recv(count: Int, flags: Int32) throws -> Data {
         var buffer = Data(count: count)
         let n = try self.unsafe { fd in
@@ -123,5 +120,153 @@ public extension SocketDescriptor where Self: ~Copyable  {
         }
         buffer.removeSubrange(n..<buffer.count)
         return buffer
+    }
+}
+
+// MARK: - CMSG Helpers (Swift replacements for macros)
+
+@inline(__always)
+private func _CMSG_ALIGN(_ len: Int) -> Int {
+    let align = MemoryLayout<Int>.size
+    return (len + align - 1) & ~(align - 1)
+}
+
+@inline(__always)
+private func CMSG_LEN(_ dataLen: Int) -> Int {
+    _CMSG_ALIGN(MemoryLayout<cmsghdr>.size) + dataLen
+}
+
+@inline(__always)
+private func CMSG_SPACE(_ dataLen: Int) -> Int {
+    _CMSG_ALIGN(MemoryLayout<cmsghdr>.size) + _CMSG_ALIGN(dataLen)
+}
+
+@inline(__always)
+private func CMSG_FIRSTHDR(_ msg: UnsafePointer<msghdr>) -> UnsafeMutablePointer<cmsghdr>? {
+    guard msg.pointee.msg_controllen >= MemoryLayout<cmsghdr>.size else { return nil }
+    return msg.pointee.msg_control?.assumingMemoryBound(to: cmsghdr.self)
+}
+
+@inline(__always)
+private func CMSG_NXTHDR(_ msg: UnsafePointer<msghdr>, _ cmsg: UnsafePointer<cmsghdr>) -> UnsafeMutablePointer<cmsghdr>? {
+    let next = UnsafeRawPointer(cmsg)
+        .advanced(by: _CMSG_ALIGN(Int(cmsg.pointee.cmsg_len)))
+    let base = msg.pointee.msg_control!
+    let end = base.advanced(by: Int(msg.pointee.msg_controllen))
+    guard next + MemoryLayout<cmsghdr>.size <= end else { return nil }
+    return UnsafeMutablePointer(mutating: next.assumingMemoryBound(to: cmsghdr.self))
+}
+
+@inline(__always)
+private func CMSG_DATA(_ cmsg: UnsafePointer<cmsghdr>) -> UnsafeMutableRawPointer {
+    UnsafeMutableRawPointer(mutating: UnsafeRawPointer(cmsg))
+        .advanced(by: _CMSG_ALIGN(MemoryLayout<cmsghdr>.size))
+}
+
+public extension SocketDescriptor where Self: ~Copyable {
+
+    func sendDescriptors<D: StreamDescriptor>(_ descriptors: [D], payload: Data = Data([0])) throws {
+        precondition(!payload.isEmpty)
+
+        try self.unsafe { sockFD in
+            var rawFDs: [Int32] = []
+            for d in descriptors {
+                d.unsafe { rawFDs.append($0) }
+            }
+
+            // Prepare the IO vector and control message
+            var iov = iovec(iov_base: UnsafeMutableRawPointer(mutating: payload.withUnsafeBytes { $0.baseAddress }),
+                            iov_len: payload.count)
+
+            var control = [UInt8](repeating: 0, count: CMSG_SPACE(rawFDs.count * MemoryLayout<Int32>.size))
+
+            // Send the payload and descriptors together
+            try payload.withUnsafeBytes { payloadPtr in
+                try control.withUnsafeMutableBytes { ctrlPtr in
+                    // Define iovPtr correctly within scope here
+                    try withUnsafeMutablePointer(to: &iov) { iovPtr in
+                        var msg = msghdr(
+                            msg_name: nil,
+                            msg_namelen: 0,
+                            msg_iov: iovPtr,
+                            msg_iovlen: 1,
+                            msg_control: ctrlPtr.baseAddress,
+                            msg_controllen: socklen_t(ctrlPtr.count),
+                            msg_flags: 0
+                        )
+
+                        guard let cmsg = CMSG_FIRSTHDR(&msg) else {
+                            fatalError("CMSG_FIRSTHDR failed")
+                        }
+
+                        cmsg.pointee.cmsg_level = SOL_SOCKET
+                        cmsg.pointee.cmsg_type = SCM_RIGHTS
+                        cmsg.pointee.cmsg_len = socklen_t(CMSG_LEN(rawFDs.count * MemoryLayout<Int32>.size))
+
+                        let dataPtr = CMSG_DATA(cmsg).assumingMemoryBound(to: Int32.self)
+                        for (i, fd) in rawFDs.enumerated() {
+                            dataPtr[i] = fd
+                        }
+
+                        let ret = Glibc.sendmsg(sockFD, &msg, 0)
+                        guard ret >= 0 else { throw POSIXError(POSIXErrorCode(rawValue: errno)!) }
+                    }
+                }
+            }
+        }
+    }
+
+    func recvDescriptors(maxDescriptors: Int = 8, bufferSize: Int = 1) throws -> (Data, [Int32]) {
+        return try self.unsafe { sockFD in
+            var buffer = [UInt8](repeating: 0, count: bufferSize)
+            let controlLen = CMSG_SPACE(maxDescriptors * MemoryLayout<Int32>.size)
+            var control = [UInt8](repeating: 0, count: controlLen)
+
+            var bytesRead = 0 // Store number of bytes read
+
+            // Make sure we return the number of bytes actually read
+            try buffer.withUnsafeMutableBytes { bufPtr in
+                try control.withUnsafeMutableBytes { ctrlPtr in
+                    // Define iov here to use it in the msghdr struct
+                    var iov = iovec(iov_base: bufPtr.baseAddress, iov_len: bufPtr.count)
+
+                    try withUnsafeMutablePointer(to: &iov) { iovPtr in
+                        var msg = msghdr(
+                            msg_name: nil,
+                            msg_namelen: 0,
+                            msg_iov: iovPtr,
+                            msg_iovlen: 1,
+                            msg_control: ctrlPtr.baseAddress,
+                            msg_controllen: socklen_t(ctrlPtr.count),
+                            msg_flags: 0
+                        )
+
+                        bytesRead = Glibc.recvmsg(sockFD, &msg, 0) // Capture bytes read here
+                        guard bytesRead >= 0 else { throw POSIXError(POSIXErrorCode(rawValue: errno)!) }
+                    }
+                }
+            }
+
+            var fds: [Int32] = []
+            control.withUnsafeBytes { ctrlPtr in
+                var msg = msghdr()
+                msg.msg_control = UnsafeMutableRawPointer(mutating: ctrlPtr.baseAddress)
+                msg.msg_controllen = socklen_t(ctrlPtr.count)
+
+                var cmsg = CMSG_FIRSTHDR(&msg)
+                while let hdr = cmsg {
+                    if hdr.pointee.cmsg_level == SOL_SOCKET && hdr.pointee.cmsg_type == SCM_RIGHTS {
+                        let count = (Int(hdr.pointee.cmsg_len) - MemoryLayout<cmsghdr>.size) / MemoryLayout<Int32>.size
+                        let dataPtr = CMSG_DATA(hdr).assumingMemoryBound(to: Int32.self)
+                        for i in 0..<count {
+                            fds.append(dataPtr[i])
+                        }
+                    }
+                    cmsg = CMSG_NXTHDR(&msg, hdr)
+                }
+            }
+
+            return (Data(buffer.prefix(bytesRead)), fds)
+        }
     }
 }
